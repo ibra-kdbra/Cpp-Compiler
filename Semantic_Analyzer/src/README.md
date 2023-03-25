8b1434aafe66b3dcdaa0fa21a34850392395a021
S.E Farouq <96024229+Ibra-22Kh@users.noreply.github.com>
2023-03-25 16:48:08 +0300

    Second and third layer

---
+++ Semantic_Analyzer/src/README.md
@@ -0,0 +1,7 @@
+
+To run the code on the terminal please enter the 'src' file then run the following line : 
+./run.sh
+
+If the file didn't run change the permission with using the line : chmod +x run.sh  //// To execute the file 
+
+Regardings, Farouq.
---
+++ Semantic_Analyzer/src/run.sh
@@ -0,0 +1,9 @@
+yacc --yacc -d 1805052-parser.y -o y.tab.cpp
+echo 'step-1: y.tab.cpp and y.tab.hpp created'
+flex -o 1805052.cpp 1805052-scanner.l
+echo 'step-2: scanner created'
+g++ -w *.cpp
+echo 'step-3: a.out created'
+rm 1805052.cpp y.tab.cpp y.tab.hpp
+./a.out input.txt
+rm a.out 
